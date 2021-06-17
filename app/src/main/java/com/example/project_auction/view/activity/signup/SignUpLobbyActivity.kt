28bee13981957b6a812fb5a7b9beade08ef46032
4538cgy <58514224+4538cgy@users.noreply.github.com>
2021-06-17 23:16:03 +0900

    commit signup lobby base

--- app/src/main/java/com/example/project_auction/view/activity/signup/SignUpLobbyActivity.kt
+++ app/src/main/java/com/example/project_auction/view/activity/signup/SignUpLobbyActivity.kt
@@ -2,11 +2,37 @@ package com.example.project_auction.view.activity.signup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.project_auction.R
+import com.example.project_auction.base.BaseActivity
+import com.example.project_auction.databinding.ActivitySignUpLobbyBinding
 
-class SignUpLobbyActivity : AppCompatActivity() {
+class SignUpLobbyActivity : BaseActivity<ActivitySignUpLobbyBinding>(R.layout.activity_sign_up_lobby) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up_lobby)
+
+        binding.activitysignuplobby = this
+
+        binding.activitySignUpLobbyImagebuttonClose.setOnClickListener { 
+            finish()
+        }
+        
+        binding.activitySignUpButtonSignUpGoogle.setOnClickListener { 
+            println("구글로 회원가입")
+        }
+        
+        binding.activitySignUpLobbyButtonSignUpKakao.setOnClickListener { 
+            println("카카오로 회원 가입")
+        }
+        
+        binding.activitySignUpLobbyButtonSignUpNaver.setOnClickListener { 
+            println("네이버로 회원 가입")
+        }
+        
+        binding.activitySignUpLobbyButtonSignUpEmail.setOnClickListener { 
+            println("이메일로 회원가입")
+        }
     }
 }
+
