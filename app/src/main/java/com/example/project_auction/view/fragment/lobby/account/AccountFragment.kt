10d7921e92ef46c44e42239b81820565989584e5
4538cgy <58514224+4538cgy@users.noreply.github.com>
2021-07-08 15:14:18 +0900

    commit lobby base

--- app/src/main/java/com/example/project_auction/view/fragment/lobby/account/AccountFragment.kt
+++ app/src/main/java/com/example/project_auction/view/fragment/lobby/account/AccountFragment.kt
@@ -6,16 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.project_auction.R
+import com.example.project_auction.base.BaseFragment
+import com.example.project_auction.databinding.FragmentAccountBinding
 
 
-class AccountFragment : Fragment() {
+class AccountFragment : BaseFragment<FragmentAccountBinding>(R.layout.fragment_account) {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.fragmentaccount = this
     }
 
 }
--- app/src/main/java/com/example/project_auction/view/fragment/lobby/auction/AuctionFragment.kt
+++ app/src/main/java/com/example/project_auction/view/fragment/lobby/auction/AuctionFragment.kt
@@ -6,22 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.project_auction.R
+import com.example.project_auction.base.BaseFragment
+import com.example.project_auction.databinding.FragmentAuctionBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-
-class AuctionFragment : Fragment() {
+class AuctionFragment : BaseFragment<FragmentAuctionBinding>(R.layout.fragment_auction) {
 
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account2, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.fragmentauction = this
     }
 
 
