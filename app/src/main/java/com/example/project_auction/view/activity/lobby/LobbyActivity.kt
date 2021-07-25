c4d2db4e36e8b4e72268c51e5feb33e9a36f8c45
4538cgy <58514224+4538cgy@users.noreply.github.com>
2021-07-25 19:15:30 +0900

    commit 바텀 시트 프로필 이미지 표시해주기

--- app/src/main/java/com/example/project_auction/view/activity/lobby/LobbyActivity.kt
+++ app/src/main/java/com/example/project_auction/view/activity/lobby/LobbyActivity.kt
@@ -1,9 +1,21 @@
 package com.example.project_auction.view.activity.lobby
 
 import android.app.AlertDialog
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuInflater
 import android.view.MenuItem
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.Request
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.target.SimpleTarget
+import com.bumptech.glide.request.target.SizeReadyCallback
+import com.bumptech.glide.request.target.Target
+import com.bumptech.glide.request.transition.Transition
 import com.example.project_auction.R
 import com.example.project_auction.base.BaseActivity
 import com.example.project_auction.databinding.ActivityLobbyBinding
@@ -15,10 +27,16 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class LobbyActivity : BaseActivity<ActivityLobbyBinding>(R.layout.activity_lobby), BottomNavigationView.OnNavigationItemSelectedListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        getProfileImage()
+
+
+
         binding.apply {
             activitylobby = this@LobbyActivity
             activityLobbyBottomNavigation.setOnNavigationItemSelectedListener(this@LobbyActivity)
             activityLobbyBottomNavigation.selectedItemId = R.id.action_auction
+
         }
     }
 
@@ -43,6 +61,46 @@ class LobbyActivity : BaseActivity<ActivityLobbyBinding>(R.layout.activity_lobby
             true
         }
         else -> false
+    }
+
+    private fun getProfileImage(){
+        db.collection("UserProfileImages").document(auth.currentUser!!.uid).get().addOnCompleteListener {
+            it?.let {
+                //none null todo
+                if (it.isSuccessful){
+                    val url = it.result!!["image"].toString()
+                    println("uid ${auth.currentUser!!.uid}")
+                    println("사진 데이터 ${url.toString()}")
+
+                    var requestBuilder = Glide.with(binding.root.context).asBitmap()
+
+                    requestBuilder.load(url).circleCrop().into(object : CustomTarget<Bitmap?>(){
+                        override fun onResourceReady(
+                            resource: Bitmap,
+                            transition: Transition<in Bitmap?>?
+                        ) {
+                            println("")
+                            var menu = binding.activityLobbyBottomNavigation.menu
+                            binding.activityLobbyBottomNavigation.itemIconTintList = null
+                            menu.findItem(R.id.action_account).setIcon(BitmapDrawable(applicationContext.resources,resource))
+                            //menu.findItem(R.id.action_account).setIcon(R.drawable.naver_icon)
+                        }
+
+                        override fun onLoadCleared(placeholder: Drawable?) {
+                            TODO("Not yet implemented")
+                        }
+                    })
+
+
+
+                }
+            }?.run {
+                //null todo
+                println("null이야")
+            }
+        }.addOnFailureListener {
+            println("실패 ${it.toString()}")
+        }
     }
 
     override fun onBackPressed() {
