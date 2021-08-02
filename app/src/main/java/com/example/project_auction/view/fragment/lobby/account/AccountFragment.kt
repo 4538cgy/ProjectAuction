70e0f493d5347e9e9e3b1d75a21c9c08c20d0598
4538cgy <58514224+4538cgy@users.noreply.github.com>
2021-08-02 17:01:05 +0900

    commit account fragment #1

--- app/src/main/java/com/example/project_auction/view/fragment/lobby/account/AccountFragment.kt
+++ app/src/main/java/com/example/project_auction/view/fragment/lobby/account/AccountFragment.kt
@@ -23,22 +23,59 @@ class AccountFragment : BaseFragment<FragmentAccountBinding>(R.layout.fragment_a
 
             fragmentAccountRecyclerSetting.adapter = SettingAdapter(
                 arrayListOf(
-                    SettingItem(SettingType.TITLE, R.drawable.ic_baseline_add_24, "제목",true),
-                    SettingItem(SettingType.BUY_LIST, R.drawable.ic_baseline_add_24, "로그아웃"),
-                    SettingItem(SettingType.TITLE, R.drawable.ic_baseline_add_24, "제목",true),
-                    SettingItem(SettingType.BUY_LIST, R.drawable.ic_baseline_add_24, "로그아웃"),
-                    SettingItem(SettingType.TITLE, R.drawable.ic_baseline_add_24, "제목",true),
-                    SettingItem(SettingType.BUY_LIST, R.drawable.ic_baseline_add_24, "로그아웃"),
-                    SettingItem(SettingType.TITLE, R.drawable.ic_baseline_add_24, "제목",true),
-                    SettingItem(SettingType.BUY_LIST, R.drawable.ic_baseline_add_24, "로그아웃"),
-                    SettingItem(SettingType.TITLE, R.drawable.ic_baseline_add_24, "제목",true),
-                    SettingItem(SettingType.BUY_LIST, R.drawable.ic_baseline_add_24, "로그아웃")
+                    SettingItem(SettingType.TITLE, R.drawable.ic_baseline_add_24, "경매", true),
+                    SettingItem(SettingType.BUY_LIST, R.drawable.ic_baseline_add_24, "경매 낙찰 내역"),
+                    SettingItem(SettingType.BUY_LIST, R.drawable.ic_baseline_add_24, "내 경매품 내역"),
+                    SettingItem(SettingType.TITLE, R.drawable.ic_baseline_add_24, "거래", true),
+                    SettingItem(SettingType.BUY_LIST, R.drawable.ic_baseline_add_24, "구매 내역"),
+                    SettingItem(SettingType.BUY_LIST, R.drawable.ic_baseline_add_24, "판매 내역"),
+                    SettingItem(SettingType.TITLE, R.drawable.ic_baseline_add_24, "혜택", true),
+                    SettingItem(SettingType.BUY_LIST, R.drawable.ic_baseline_add_24, "이벤트"),
+                    SettingItem(SettingType.TITLE, R.drawable.ic_baseline_add_24, "더보기", true),
+                    SettingItem(SettingType.BUY_LIST, R.drawable.ic_baseline_add_24, "앱 설정"),
+                    SettingItem(SettingType.BUY_LIST, R.drawable.ic_baseline_add_24, "계정 설정"),
+                    SettingItem(SettingType.BUY_LIST, R.drawable.ic_baseline_add_24, "공지사항"),
+                    SettingItem(SettingType.BUY_LIST, R.drawable.ic_baseline_add_24, "문의하기")
 
-                )
+                    )
             ).setOnSettingItemClickListener {
+                when(this.title){
+                    "경매 낙찰 내역" ->{
+
+                    }
+                    "내 경매품 내역" ->{
+
+                    }
+                    "구매 내역" ->{
+
+                    }
+                    "판매 내역" ->{
+
+                    }
+                    "이벤트" ->{
+
+                    }
+                    "앱 설정" ->{
+
+                    }
+                    "계정 설정"->{
+
+                    }
+                    "공지사항" ->{
+
+                    }
+                    "문의하기" ->{
+
+                    }
+                }
+            }
+            fragmentAccountRecyclerSetting.layoutManager =
+                LinearLayoutManager(root.context, LinearLayoutManager.VERTICAL, false)
+
+            fragmentAccountConstraintNicknameContainer.setOnClickListener {
+                //프로필 변경 todo
 
             }
-            fragmentAccountRecyclerSetting.layoutManager = LinearLayoutManager(root.context,LinearLayoutManager.VERTICAL,false)
         }
 
         //닉네임 옵저빙
